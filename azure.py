4217d86a8494ae087070a41bb1902ad7dc93b497
WCS19 <wcs@bu.edu>
2023-10-16 17:07:50 -0400

    temperature slider added

--- azure.py
+++ azure.py
@@ -14,6 +14,9 @@ st.title("Ask GPT-3 a question")
 # Create a text input field for user queries
 user_input = st.text_input("Ask a question:")
 
+#Create temperature slider
+temperature = st.slider("Temperature", 0.1, 1.0, 0.7)
+
 # Send the user's query to OpenAI GPT-3
 if user_input:
     response = openai.Completion.create(
